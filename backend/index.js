c01659d5da9e6daf39c1966f31bf2b4ab63761a9
Animesh Naskar <an2190944@gmail.com>
2026-05-12 00:43:21 +0530

    update backend and fix api

--- backend/index.js
+++ backend/index.js
@@ -310,7 +310,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://whimsical-mandazi-be2e85.netlify.app",
+    origin: "https://login-registero.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
